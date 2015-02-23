2f425264172d7053539488305e382c9490936a97
Callum Taylor <callum@3sidedcube.com>
2015-02-23 11:22:56 +0000

    Now we create a basic activity using the layout we created

---
+++ app/src/main/java/com/cube/bbcnews/MainActivity.java
@@ -0,0 +1,20 @@
+package com.cube.bbcnews;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * // TODO: Add class description
+ *
+ * @author Callum Taylor
+ * @project BBCNews
+ */
+public class MainActivity extends Activity
+{
+	@Override protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.main_view);
+	}
+}
