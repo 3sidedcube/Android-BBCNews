51cb1148ddc2456c2aa3168c43b27a077ad698f1
Callum Taylor <callum@3sidedcube.com>
2015-02-23 11:25:10 +0000

    Now we add reference to the activity into the manifest so the operating system knows that it exists and that it is the default activity to launch when you tap on your app icon

--- app/src/main/java/com/cube/bbcnews/MainActivity.java
+++ app/src/main/java/com/cube/bbcnews/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends Activity
 	@Override protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		
+
 		setContentView(R.layout.main_view);
 	}
 }
