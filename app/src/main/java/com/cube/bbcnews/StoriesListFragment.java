c2618feb435c67db38c527d15039c1b6d24334cc
Callum Taylor <callum@3sidedcube.com>
2015-03-05 14:28:08 +0000

    Added basic list view

--- app/src/main/java/com/cube/bbcnews/StoriesListFragment.java
+++ app/src/main/java/com/cube/bbcnews/StoriesListFragment.java
@@ -6,6 +6,8 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * // TODO: Add class description
@@ -15,9 +17,38 @@ import android.view.ViewGroup;
  */
 public class StoriesListFragment extends Fragment
 {
+	private ListView listView;
+
 	@Nullable @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 	{
 		View view = inflater.inflate(R.layout.stories_list_view, container, false);
+		listView = (ListView)view.findViewById(R.id.list_view);
+
 		return view;
+	}
+
+	@Override public void onActivityCreated(Bundle savedInstanceState)
+	{
+		super.onActivityCreated(savedInstanceState);
+
+		String[] names = {
+			"Callum",
+			"Ollie",
+			"Steve",
+			"Tim",
+			"Matt",
+			"Matt C",
+			"Keisha",
+			"Duncan",
+			"Jess",
+			"Sam",
+			"Simon",
+			"Dan",
+			"Puff",
+			"Sophie",
+			"Imogen"
+		};
+
+		listView.setAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, names));
 	}
 }
