7f1157441704e06064e2b4a3b776cf155a289b54
Callum Taylor <callum@3sidedcube.com>
2015-03-05 14:39:46 +0000

    Set list to use custom adapter

--- app/src/main/java/com/cube/bbcnews/StoriesListFragment.java
+++ app/src/main/java/com/cube/bbcnews/StoriesListFragment.java
@@ -6,7 +6,6 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 /**
@@ -49,6 +48,6 @@ public class StoriesListFragment extends Fragment
 			"Imogen"
 		};
 
-		listView.setAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, names));
+		listView.setAdapter(new StoriesAdapter(getActivity(), names));
 	}
 }
