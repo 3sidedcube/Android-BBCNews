a4fbeca4048d62c9a723d5e447303b0f9164c0b4
Callum Taylor <callum@3sidedcube.com>
2015-03-10 10:33:55 +0000

    Added layout inflation for story details fragment

--- app/src/main/java/com/cube/bbcnews/StoryDetailsFragment.java
+++ app/src/main/java/com/cube/bbcnews/StoryDetailsFragment.java
@@ -1,8 +1,28 @@
 package com.cube.bbcnews;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class StoryDetailsFragment extends Fragment
 {
-	
+	private ImageView featureImage;
+	private TextView header;
+	private TextView story;
+
+	@Nullable @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+	{
+		View view = inflater.inflate(R.layout.story_detail_view, container, false);
+
+		featureImage = (ImageView)view.findViewById(R.id.feature_image);
+		header = (TextView)view.findViewById(R.id.header);
+		story = (TextView)view.findViewById(R.id.story);
+
+		return view;
+	}
 }
