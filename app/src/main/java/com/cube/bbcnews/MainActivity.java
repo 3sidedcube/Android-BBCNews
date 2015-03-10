54bf89f5ef1d55c1a6f3ab45790d84e61453f9c5
Callum Taylor <callum@3sidedcube.com>
2015-03-10 11:58:57 +0000

    Added select story method

--- app/src/main/java/com/cube/bbcnews/MainActivity.java
+++ app/src/main/java/com/cube/bbcnews/MainActivity.java
@@ -12,6 +12,8 @@ import android.os.Bundle;
  */
 public class MainActivity extends Activity
 {
+	private String story;
+
 	@Override protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
@@ -21,9 +23,28 @@ public class MainActivity extends Activity
 		FragmentTransaction transaction = getFragmentManager().beginTransaction();
 		transaction.replace(R.id.fragment_holder, new StoriesListFragment());
 
+		transaction.commit();
+	}
+
+	public void selectStory(String story)
+	{
+		this.story = story;
+
+		FragmentTransaction transaction = getFragmentManager().beginTransaction();
+
+		Bundle extras = new Bundle();
+		extras.putString("name", story);
+
+		StoryDetailsFragment fragment = new StoryDetailsFragment();
+		fragment.setArguments(extras);
+
 		if (findViewById(R.id.fragment2_holder) != null)
 		{
-			transaction.replace(R.id.fragment2_holder, new StoriesListFragment());
+			transaction.replace(R.id.fragment2_holder, fragment);
+		}
+		else
+		{
+			transaction.replace(R.id.fragment_holder, fragment).addToBackStack("story");
 		}
 
 		transaction.commit();
