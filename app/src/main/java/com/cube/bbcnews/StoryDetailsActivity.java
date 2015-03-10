2963778c1f1c511d880422e382f37bcd2fbc7129
Callum Taylor <callum@3sidedcube.com>
2015-03-10 10:56:29 +0000

    Added story details activity and fragment inflation

---
+++ app/src/main/java/com/cube/bbcnews/StoryDetailsActivity.java
@@ -0,0 +1,19 @@
+package com.cube.bbcnews;
+
+import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+
+public class StoryDetailsActivity extends Activity
+{
+	@Override protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.fragment_holder_view);
+
+		FragmentTransaction transaction = getFragmentManager().beginTransaction();
+		transaction.replace(R.id.fragment_holder, new StoryDetailsFragment());
+		transaction.commit();
+	}
+}
